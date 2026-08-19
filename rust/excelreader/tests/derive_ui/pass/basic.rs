db958a13e772f7ff944ffe53c59d869fad1ab077
GabrielMarquezMatte <gabriel.matte@peakinvest.com.br>
2026-08-19 20:54:07 -0300

    Fixing rust tests

--- rust/excelreader/tests/derive_ui/pass/basic.rs
+++ rust/excelreader/tests/derive_ui/pass/basic.rs
@@ -16,12 +16,12 @@ struct Row {
 fn main() {
     let bindings = Row::bindings();
     assert_eq!(bindings.len(), 4);
-    assert_eq!(bindings[0].name, "Nome");
+    assert_eq!(bindings[0].names[0], "Nome");
     assert_eq!(bindings[0].xl_type, XL_T_STRING);
-    assert_eq!(bindings[1].name, "Idade");
+    assert_eq!(bindings[1].names[0], "Idade");
     assert_eq!(bindings[1].xl_type, XL_T_I64);
-    assert_eq!(bindings[2].name, "Peso");
+    assert_eq!(bindings[2].names[0], "Peso");
     assert_eq!(bindings[2].xl_type, XL_T_F64);
-    assert_eq!(bindings[3].name, "Ativo");
+    assert_eq!(bindings[3].names[0], "Ativo");
     assert_eq!(bindings[3].xl_type, XL_T_BOOL);
 }
